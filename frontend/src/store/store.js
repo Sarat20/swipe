b6c591369e09cf2f7b60d82df0079568a4b06db9
Sarat20 <saratkumarkalagara2398s@gmail.com>
2025-09-26 23:19:05 +0530

    updated persistconfig in store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import candidateReducer from "./candidateSlice";
 
-const persistConfig = { key: "root", storage };
-const persistedReducer = persistReducer(persistConfig, candidateReducer);
 
-export const store = configureStore({ reducer: persistedReducer });
+const persistConfig = { key: "candidates", storage };
+const candidatesPersistedReducer = persistReducer(persistConfig, candidateReducer);
+
+export const store = configureStore({
+  reducer: {
+    candidates: candidatesPersistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
 export const persistor = persistStore(store);
