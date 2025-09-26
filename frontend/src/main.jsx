9d65239f34377f5eeb5b4cc960db8822e94f3fbc
Sarat20 <saratkumarkalagara2398s@gmail.com>
2025-09-26 19:35:58 +0530

    updated main for statemanagement and persistence

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store/store.js";   // our Redux store
+import { PersistGate } from "redux-persist/integration/react";
+import "antd/dist/reset.css";   // Ant Design styles
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
