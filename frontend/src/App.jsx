af3af00714c472c10ef5198b59779747e2145930
Sarat20 <saratkumarkalagara2398s@gmail.com>
2025-09-26 19:42:26 +0530

    added pages interviewee and interviewer in pages ,also updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Tabs, Layout } from "antd";
+import Interviewee from "./pages/Interviewee";
+import Interviewer from "./pages/Interviewer";
 
-const App = () => {
+const { TabPane } = Tabs;
+const { Content } = Layout;
+
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <Layout style={{ minHeight: "100vh", padding: 24 }}>
+      <Content>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Interviewee" key="1">
+            <Interviewee />
+          </TabPane>
+          <TabPane tab="Interviewer Dashboard" key="2">
+            <Interviewer />
+          </TabPane>
+        </Tabs>
+      </Content>
+    </Layout>
+  );
 }
- 
-export default App
---
+++ frontend/src/pages/Interviewee.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Interviewee = () => {
+  return (
+    <div>Interviewee</div>
+  )
+}
+
+export default Interviewee
